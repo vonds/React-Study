9f162396f3ed35f747ac78579ae4f1896d063cab
Vonds <vonds13@gmail.com>
2018-12-19 22:53:41 -0500

    components lesson

---
+++ components/src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <CommentDetail
+                    author="Chi Chi"
+                    timeAgo="Today at 4:45PM"
+                    content="Nice blog post"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Raditz"
+                    timeAgo="Today at 2:00AM"
+                    content="I like the subject"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Bulma"
+                    timeAgo="Yesterday at 5:00PM"
+                    content="I like the writing"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+        </div>
+    );
+};
+ReactDOM.render(<App />, document.querySelector('#root'))
